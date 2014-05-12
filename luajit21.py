cca736d16cf2ded4b165b133b2a7df522f0e5656
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-05-12 15:15:10 -0700

    luajit21.py: support the saved_jit_base in my fork of luajit2 (for the context of lj_err_throw).

--- luajit21.py
+++ luajit21.py
@@ -506,6 +506,12 @@ Usage: lbt [L]
             T = traceref(J, traceno)
             base = tvref(g['jit_base'])
             if not base:
+                try:
+                    base = tvref(g['saved_jit_base'])
+                except:
+                    pass
+
+            if not base:
                 raise gdb.GdbError("jit base is NULL (trace #%d)" % int(T['traceno']))
             bt = lj_debug_dumpstack(L, T, 30, base, full)
 
